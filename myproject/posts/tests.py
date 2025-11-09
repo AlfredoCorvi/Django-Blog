a881c75d61a038f9ca5f44ca7a4cf747ab8b3e5f
AlfredoCorvi <al.cor.vi05@gmail.com>
2025-11-09 15:43:31 -0600

    Post test added

--- myproject/posts/tests.py
+++ myproject/posts/tests.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+class PostModelTest(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username="testuser", password="secret")
+        Post.objects.create(title="Test Post", body="This is a test post.", author=user)
+
+    def test_post_content(self):
+        post = Post.objects.get(id=1)
+        self.assertEqual(post.title, "Test Post")
+        self.assertEqual(post.body, "This is a test post.")
+        self.assertEqual(post.author.username, "testuser")
